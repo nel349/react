06ebc9a0c45f9a44db9484bb6701507246003fbc
Norman Lopez <noell.lpz@gmail.com>
2022-05-03 17:43:25 -0700

    completed Lists in React

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,13 +1,51 @@
 import * as React from 'react';
 
-let title = "REACT!";
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+];
+
+function addFromItem(item) {
+  return (
+    <li key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </li>
+  );
+}
 
 function App() {
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>My Hacker Stories</h1>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text"/>
+      <input id="search" type="text" />
+
+      <hr />
+
+      <ul>
+        {
+          list.map(item => addFromItem(item))
+        }
+      </ul>
 
     </div>
   );
