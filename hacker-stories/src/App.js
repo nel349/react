a6820860caf1e0bab7e4d6f6a8637cbe7a4e381f
Norman Lopez <noell.lpz@gmail.com>
2022-05-17 08:58:06 -0700

    completed React conditional rendering

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -113,11 +113,16 @@ const App = () => {
     );
 
   const [stories, setStories] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
 
   React.useEffect(() => {
+    setIsLoading(true);
     getAsyncStories().then(result => {
       setStories(result.data.stories);
-    });
+      setIsLoading(false);
+    })
+    .catch(() => setIsError(true));
   }, []);
 
   const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
@@ -154,8 +159,11 @@ const App = () => {
 
       <hr />
 
-      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      {isError && <p>Something went wrong ...</p>}
 
+      {isLoading ? (<p> Loading ... </p>) : (
+        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      )}
     </div>
   );
 }
