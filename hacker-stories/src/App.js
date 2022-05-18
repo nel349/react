657ce0b27825e09a6c00207b587aa502d10e39ab
Norman Lopez <noell.lpz@gmail.com>
2022-05-18 13:49:37 -0400

    completed React Impossible state

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -114,11 +114,32 @@ const App = () => {
 
   const storiesReducer = (state, action) => {
     switch (action.type) {
-      case 'SET_STORIES':
-        return action.payload;
-      case 'REMOVE_STORY':
-        return state.filter(
-          (story) => action.payload.objectID !== story.objectID);
+case 'STORIES_FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case 'STORIES_FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case 'STORIES_FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    case 'REMOVE_STORY':
+      return {
+        ...state,
+        data: state.data.filter(
+          (story) => action.payload.objectID !== story.objectID
+        ),
+};
       default:
         throw new Error();
     }
@@ -126,24 +147,19 @@ const App = () => {
     
   const [stories, dispatchStories] = React.useReducer(
     storiesReducer,
-    []
+    { data: [], isLoading: false, isError: false }
   );
   
-  const [isLoading, setIsLoading] = React.useState(false);
-  const [isError, setIsError] = React.useState(false);
 
   React.useEffect(() => {
-    setIsLoading(true);
     getAsyncStories().then(result => {
-      // setStories(result.data.stories);
       dispatchStories({
-        type: 'SET_STORIES',
+        type: 'STORIES_FETCH_SUCCESS',
         payload: result.data.stories,
       });
-      setIsLoading(false);
     })
-    .catch(() => setIsError(true));
-  }, []);
+    .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
+  });
 
   const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
 
@@ -160,7 +176,7 @@ const App = () => {
     setSearchTerm(event.target.value);
   };
 
-  const searchedStories = stories.filter((story) => {
+  const searchedStories = stories.data.filter((story) => {
     return story.title.toLowerCase().includes(searchTerm.toLowerCase())
       || story.author.toLowerCase().includes(searchTerm.toLowerCase())
   }
@@ -179,9 +195,9 @@ const App = () => {
 
       <hr />
 
-      {isError && <p>Something went wrong ...</p>}
+      {stories.data.isError && <p>Something went wrong ...</p>}
 
-      {isLoading ? (<p> Loading ... </p>) : (
+      {stories.data.isLoading ? (<p> Loading ... </p>) : (
         <List list={searchedStories} onRemoveItem={handleRemoveStory} />
       )}
     </div>
